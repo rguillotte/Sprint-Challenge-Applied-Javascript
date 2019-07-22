a0b4842765cbf38d267b10557e7c955895b61d9d
Ray Guillotte <rayguillotte@Rays-Air.fios-router.home>
2019-07-21 23:06:05 -0400

    finished articles component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,73 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(data=>{
+        // console.log(data);
+
+        const bootstrapInfo = data.data.articles.bootstrap;
+        bootstrapInfo.forEach((i=>{
+            createCard(i);
+        }))
+
+        const javascriptInfo = data.data.articles.javascript;
+        javascriptInfo.forEach((i=>{
+            createCard(i);
+        }))
+
+        const jqueryInfo = data.data.articles.jquery;
+        jqueryInfo.forEach((i=>{
+            createCard(i);
+        }))
+
+        const nodeInfo = data.data.articles.node;
+        nodeInfo.forEach((i=>{
+            createCard(i);
+        }))
+
+        const technologyInfo = data.data.articles.technology;
+        technologyInfo.forEach((i=>{
+            createCard(i);
+        }))
+
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+
+function createCard(data){
+    //create elements
+    cardDiv = document.createElement('div');
+    headline = document.createElement('div');
+    author = document.createElement('div');
+    imgContainer = document.createElement('div');
+    image = document.createElement('img');
+    byLine = document.createElement('span');
+
+    //add classes
+    cardDiv.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    //put together
+    cardDiv.appendChild(headline);
+    cardDiv.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(image);
+    author.appendChild(byLine);
+
+    const cardsContainer = document.querySelector('.cards-container');
+
+    cardsContainer.appendChild(cardDiv);
+
+    //add content
+    headline.textContent = data.headline;
+    byLine.textContent = `By: ${data.authorName}`;
+    image.setAttribute('src', data.authorPhoto);
+
+    //return
+    return cardDiv;
+
+}
